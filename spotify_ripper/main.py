52114c05fbb21a6cac8f0b68fdecaeb7cbc99c98
Simone Caronni <negativo17@gmail.com>
2020-06-13 09:19:35 +0200

    Use is_alive

--- spotify_ripper/main.py
+++ spotify_ripper/main.py
@@ -611,7 +611,7 @@ def main(prog_args=sys.argv[1:]):
         if not args.has_log and stdin_settings:
             tty.setcbreak(sys.stdin.fileno())
 
-        while ripper.isAlive():
+        while ripper.is_alive():
             schedule.run_pending()
 
             # check if the escape button was pressed
