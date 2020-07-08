6dd5d78d5086d6e9e81d4938a1401c6df03307fa
Simone Caronni <negativo17@gmail.com>
2020-07-08 11:36:50 +0200

    Update to 2.14

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def _read(fn):
 
 setup(
     name='spotify-ripper',
-    version='2.13',
+    version='2.14',
     packages=find_packages(exclude=["tests"]),
     #scripts=['spotify_ripper/main.py'],
     include_package_data=True,
@@ -53,7 +53,7 @@ setup(
     license='MIT',
     keywords="spotify ripper mp3 ogg vorbis flac opus acc mp4 m4a",
     url='https://github.com/scaronni/spotify-ripper',
-    download_url='https://github.com/scaronni/spotify-ripper/tarball/2.13',
+    download_url='https://github.com/scaronni/spotify-ripper/tarball/2.14',
     classifiers=[
         'Topic :: Multimedia :: Sound/Audio',
         'Topic :: Multimedia :: Sound/Audio :: Capture/Recording',
