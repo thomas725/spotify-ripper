7aa4c38a96b6b45caf0d328e9c5eba6a3015dce3
iRet <iRet@a22.in>
2021-03-13 04:45:33 +0700

    Bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def _read(fn):
 
 setup(
     name='spotify-ripper',
-    version='2.15',
+    version='2.17',
     packages=find_packages(exclude=["tests"]),
     #scripts=['spotify_ripper/main.py'],
     include_package_data=True,
